6fb93b462ab3a4e5a0d1173bf41269b09a994879
root <mariamnarebarkenoaga@gmail.com>
2023-03-17 11:32:30 -0700

    my project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
-int main() {
-   /* my first program in C */
-   printf("Hello, World! \n");
-   
-   return 0;
+main() {
+
+   int a = 21;
+   int b = 10;
+   int c ;
+
+   c = a + b;
+   printf("Line 1 - Value of c is %d\n", c );
+	
+   c = a - b;
+   printf("Line 2 - Value of c is %d\n", c );
+	
+   c = a * b;
+   printf("Line 3 - Value of c is %d\n", c );
+	
+   c = a / b;
+   printf("Line 4 - Value of c is %d\n", c );
+	
+   c = a % b;
+   printf("Line 5 - Value of c is %d\n", c );
+	
+   c = a++; 
+   printf("Line 6 - Value of c is %d\n", c );
+	
+   c = a--; 
+   printf("Line 7 - Value of c is %d\n", c );
 }
