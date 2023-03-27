bb0511d9abab4fd7345ba77773f7a69e39aafc35
Marimnarebarkenoaga <mariamnarebarkenoaga@gmail.com>
2023-03-27 16:34:07 -0700

    my project

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line
+ *
+ * @str : The variable that store the stirng
+ *
+ * Return : Nothing
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
